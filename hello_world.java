0146503112526e291a6c73bbc1938625128da37c
carterbk1438 <carterbk@gmail.com>
2013-02-17 22:54:19 -0500

    second commit

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+/* This is my first Windows java program
+ *
+ * type javac hello_world to compile 
+ * type java hello_world to run
+ */
+
+class hello_world  {
+	public static void main(String[] args) {
+		System.out.println("What's up world?");
+	}
+
+}
